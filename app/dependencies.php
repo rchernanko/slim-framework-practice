20e4fe20704ea883b08902b0112b12241f9bda74
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-01-04 08:39:31 +0000

    added exercise and vote classes to the DI container - not too sure on this though, I would want to talk this through

--- app/dependencies.php
+++ app/dependencies.php
@@ -4,18 +4,32 @@
 use BusuuTest\EntityRepository\ExerciseRepository;
 use BusuuTest\EntityRepository\UserRepository;
 use BusuuTest\Controller\ExerciseController;
+use BusuuTest\Entity\Exercise;
+use BusuuTest\Entity\Vote;
 
 $container = $app->getContainer();
 
-$container[ExerciseRepository::class] = function() {
+//TODO not sure exactly if this is correct - talk it through...
+$container[Vote::class] = function () {
+    return new Vote();
+};
+
+//TODO not sure exactly if this is correct - talk it through...
+$container[Exercise::class] = function () use ($container) {
+    return new Exercise(
+        $container[Vote::class]
+    );
+};
+
+$container[ExerciseRepository::class] = function () {
     return new ExerciseRepository();
 };
 
-$container[UserRepository::class] = function() {
+$container[UserRepository::class] = function () {
     return new UserRepository();
 };
 
-$container[ExerciseController::class] = function() use ($container) {
+$container[ExerciseController::class] = function () use ($container) {
     return new ExerciseController(
         $container[ExerciseRepository::class],
         $container[UserRepository::class]

78ba5395077f5de60a02f48fa8e9b0bb1a09b037
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-01-24 21:01:54 +0000

    adding the require once for the dbConnect file

--- public/index.php
+++ public/index.php
@@ -6,5 +6,6 @@ $app = new \Slim\App();
 
 require_once __DIR__ . '/../app/dependencies.php';
 require_once __DIR__ . '/../app/routes.php';
+require_once __DIR__ . '/../database/dbConnect.php';
 
 $app->run();

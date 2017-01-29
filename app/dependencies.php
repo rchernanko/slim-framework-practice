e1768ebb97da1e9f370629680be6dee8630cf0cd
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-01-29 21:15:47 +0000

    Adding the db details to my dependencies file and also adding a few comments explaining how that file works

--- app/dependencies.php
+++ app/dependencies.php
@@ -7,6 +7,26 @@ use BusuuTest\Controller\ExerciseController;
 
 $container = $app->getContainer();
 
+//Add my db connection to the dependency container
+$container['db'] = function ($container) {
+    $config = $container['config']['db'];
+    return new mysqli($config['host'], $config['user'], $config['pass'], $config['db_name']);
+};
+
+//Now let's add the 3 classes I need as part of the MVC pattern:
+
+//ExerciseController, ExerciseRepository and UserRepository
+
+/*
+
+NOTE the below will also work for exerciseRepository
+
+$container['exerciseRepository'] = function () {
+    return new ExerciseRepository();
+};
+
+*/
+
 $container[ExerciseRepository::class] = function() {
     return new ExerciseRepository();
 };
@@ -20,9 +40,4 @@ $container[ExerciseController::class] = function() use ($container) {
         $container[ExerciseRepository::class],
         $container[UserRepository::class]
     );
-};
-
-$container['db'] = function ($container) {
-    $config = $container['config']['db'];
-    return new mysqli($config['host'], $config['user'], $config['pass'], $config['db_name']);
 };

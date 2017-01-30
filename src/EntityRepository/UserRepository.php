d6d69550a243137d3f029a27eb3ab2a2013e2731
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-01-30 08:54:10 +0000

    updating empty user repository class with inherited methods from repositoryinterface

--- src/EntityRepository/UserRepository.php
+++ src/EntityRepository/UserRepository.php
@@ -4,15 +4,32 @@
 namespace BusuuTest\EntityRepository;
 
 
+use Slim\Container;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 class UserRepository implements RepositoryInterface
 {
-    public function find($userId)
+    public function find($userId, Response $response, Container $container)
     {
-        // ...
     }
 
-    public function save($user)
+    public function findAll(Response $response, Container $container)
     {
-        // ...
     }
+
+    public function save(Request $request, Response $response, Container $container)
+    {
+    }
+
+    public function delete($entityId, Response $response, Container $container)
+    {
+    }
+
+    public function update(Request $request, Response $response, Container $container)
+    {
+    }
+
+    //TODO come back to this when I start to look at ORM
+    //public function save($user){}
 }

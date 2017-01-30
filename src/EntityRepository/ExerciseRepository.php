1ac705388de1ae21936720ea997debe89fa0dba6
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-01-30 08:57:59 +0000

    Refactored the exercise repository class and added a load of todos

--- src/EntityRepository/ExerciseRepository.php
+++ src/EntityRepository/ExerciseRepository.php
@@ -1,15 +1,127 @@
 <?php
 namespace BusuuTest\EntityRepository;
 
+use Slim\Container;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 class ExerciseRepository implements RepositoryInterface
 {
-    public function find($exerciseId)
+    public function findAll(Response $response, Container $container)
     {
-        // ...
+        $mysqli = $container['db']; //TODO how can I reduce duplication of getting the container on every function...?
+
+        $query = "select * from exercises";
+        $result = $mysqli->query($query);
+
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+
+        if (isset($data)) {
+            return $response->withJson($data); //TODO add a status code too
+        }
+
+        return $response->withJson(['msg' => 'No exercises found'], 404);
+
+        //TODO make more robust...what if the query fails for example?
+        //TODO I should probably make this a prepared statement too
     }
 
-    public function save($exercise)
+    public function find($exerciseId, Response $response, Container $container)
     {
-        // ...
+        $mysqli = $container['db'];
+
+        $query = "select * from exercises where exerciseId = $exerciseId";
+        $result = $mysqli->query($query);
+
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+
+        if (isset($data)) {
+            return $response->withJson($data); //TODO add a status code too
+        }
+
+        return $response->withJson(['msg' => 'No exercises found'], 404);
+
+        //TODO make more robust...what if the query fails for example?
+        //TODO I should probably make this a prepared statement too
+    }
+
+    public function delete($exerciseId, Response $response, Container $container)
+    {
+        $mysqli = $container['db'];
+
+        $query = "delete from exercises where exerciseId = $exerciseId";
+        $mysqli->query($query);
+
+        return $response->withJson("Exercise with id $exerciseId has been deleted");
+        //TODO add a status code too
+        //TODO what if the above query fails to execute? Or what if that exercise doesn't even exist? Make more robust
+        //TODO something like the below?
+
+        /*
+         *
+         * $query = mysqli_query($con, "SELECT * FROM emails WHERE email='".$email."'");
+
+            if(mysqli_num_rows($query) > 0){
+
+            echo "email already exists";
+                }else{
+                  // do something
+                  if (!mysqli_query($con,$query))
+                  {
+                 die('Error: ' . mysqli_error($con));
+          }
+            }
+
+         */
+    }
+
+    public function save(Request $request, Response $response, Container $container)
+    {
+        $mysqli = $container['db'];
+
+        $query = "INSERT INTO exercises (exerciseId, author, exerciseText) VALUES (?,?,?)";
+
+        $stmt = $mysqli->prepare($query);
+
+        $stmt->bind_param("iss", $exerciseId, $author, $exerciseText);
+
+        $exerciseId = $request->getParsedBody()['exerciseId'];
+        $author = $request->getParsedBody()['author'];
+        $exerciseText = $request->getParsedBody()['exerciseText'];
+
+        $stmt->execute();
+
+        return $response->withJson("Exercise $exerciseId has been created");
+
+        //TODO add a status code too
+        //TODO what if the above query fails to execute? Make more robust
+    }
+
+    public function update(Request $request, Response $response, Container $container)
+    {
+        $mysqli = $container['db'];
+
+        $id = $request->getAttribute('id');
+
+        $query = "UPDATE exercises SET exerciseId = ?, author = ?, exerciseText = ? WHERE exercises.exerciseId = $id";
+
+        $stmt = $mysqli->prepare($query);
+
+        $stmt->bind_param("iss", $exerciseId, $author, $exerciseText);
+
+        $exerciseId = $request->getParsedBody()['exerciseId'];
+        $author = $request->getParsedBody()['author'];
+        $exerciseText = $request->getParsedBody()['exerciseText'];
+
+        $stmt->execute();
+
+        return $response->withJson("Exercise text within $id has been updated");
+
+        //TODO add a status code too
+        //TODO what if the above query fails to execute? Make more robust
     }
 }

d387bd01712db621e7f41ea32b247bdfa05a9200
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-01-30 08:38:19 +0000

    removing a few redundant todos from the config file

--- app/config.php
+++ app/config.php
@@ -15,7 +15,3 @@ $container = $app->getContainer();
 
 //Add config to the container to make it accessible
 $container['config'] = $config;
-
-//TODO read a little more re: the container and DIC in slim framework
-
-//TODO get above running not on phpmyadmin and XAMMP but on an apache server / db without XAMMP

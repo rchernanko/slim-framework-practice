7ff04261364d7a99a639447875f5b94bf8e00086
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-01-30 08:53:34 +0000

    updating repository interface

--- src/EntityRepository/RepositoryInterface.php
+++ src/EntityRepository/RepositoryInterface.php
@@ -2,15 +2,43 @@
 namespace BusuuTest\EntityRepository;
 
 
+use Slim\Container;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 interface RepositoryInterface
 {
     /**
-     * Return an entity object or null if the id does not exist
+     * Return an entity object or null if the id does not exist //TODO make my all of below responses a lot more robust...
      *
      * @param $entityId
-     * @return mixed
+     * @param Response $response
+     * @param Container $container
+     * @return
      */
-    public function find($entityId);
+    public function find($entityId, Response $response, Container $container);
+
+    /**
+     * Return all entity objects or null if none exist
+     *
+     * @param Response $response
+     * @param Container $container
+     * @return
+     */
+    public function findAll(Response $response, Container $container);
+
+    /**
+     * Delete a specific entity object
+     * @param $entityId
+     * @param Response $response
+     * @param Container $container
+     * @return
+     */
+    public function delete($entityId, Response $response, Container $container);
+
+    public function save(Request $request, Response $response, Container $container);
+
+    public function update(Request $request, Response $response, Container $container);
 
     /**
      * Save entity to database
@@ -18,5 +46,7 @@ interface RepositoryInterface
      * @param $entityObject
      * @return mixed
      */
-    public function save($entityObject);
+    //public function save($entityObject);
+    //TODO come back to this when I start to look at ORM. This way should replace all of the above routes.
+    //TODO i.e. i should be updating, deleting, creating via Entities, not via direct SQL commands to the database
 }

69a13f5e1862d78b5f70db6ee5095ae59a33b9f4
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-02-07 13:36:37 +0000

    adding todo

--- src/EntityRepository/ExerciseRepository.php
+++ src/EntityRepository/ExerciseRepository.php
@@ -8,6 +8,7 @@ class ExerciseRepository extends Repository
 {
 
     //TODO abstract the duplicated code from below into some sort of support class
+    //TODO change responses to ensure best practice as per 'how to build apis that people won't hate book"
 
     public function findAll()
     {

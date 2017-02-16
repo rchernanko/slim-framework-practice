a5ed150d97973e11c550791345631f757c54e1c9
Richard Chernanko <rchernanko@hotmail.co.uk>
2017-02-16 14:17:01 +0000

    Adding a todo in the exerciserepository class

--- src/entityRepository/Repository.php
+++ src/entityRepository/Repository.php
@@ -6,6 +6,10 @@ use Slim\Container;
 
 abstract class Repository implements RepositoryInterface
 {
+    /*
+     * TODO think about this. Similar to the ExerciseController, this class doesn't need access to the whole container,
+     * TODO just certain things in it (e.g. the db connection) - So re-work so that it only has access to what it needs
+     */
     protected $container;
 
     /**
